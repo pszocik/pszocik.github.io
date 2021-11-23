c22dc5601ba09f07bbcd789743ad2a6772e56d27
Piotr Szocik <piotrek.szocik@gmail.com>
2021-11-23 21:00:33 +0100

    fix

--- scripts/script.js
+++ scripts/script.js
@@ -3,7 +3,6 @@ import appData from "./apps.js";
 const appCardButtons = document.querySelectorAll(".card");
 
 appCardButtons.forEach((el, i) => {
-    console.log(typeof appData[i] === 'undefined')
     if(typeof appData[i] === 'undefined') return;
     el.innerHTML = appData[i].name;
     el.addEventListener("click", () => {
