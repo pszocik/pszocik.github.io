60c5dd153cdb6ec22e5a553e241829dd319c724b
Piotr Szocik <piotrek.szocik@gmail.com>
2021-11-23 16:37:38 +0100

    .

--- src/Project/Project.js
+++
@@ -1,9 +0,0 @@
-const Project = ({ project }) => {
-  return (
-    <a target="_blank" rel="noopener noreferrer" href={project.url}>
-      {project.name}
-    </a>
-  );
-};
-
-export default Project;
